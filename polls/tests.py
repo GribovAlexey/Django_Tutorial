84eaafcf9d5e7c69f3ef4fbd60edc9487fe9e79f
GribovAlexey <gribov.alexey.m@yandex.ru>
2022-05-31 15:28:35 +0300

    Task 5. Part 1. Fixed test 3

--- polls/tests.py
+++ polls/tests.py
@@ -18,7 +18,7 @@ class QuestionModelTests(TestCase):
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
-        time = timezone.now() + datetime.timedelta(hours=23, minutes=59,
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59,
                                                    seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
