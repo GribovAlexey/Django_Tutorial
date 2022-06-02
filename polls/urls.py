a55354377a801f2557bef8b3fc54f65a523de010
Gribov Alexey <a.gribov@netrika.ru>
2022-06-02 12:26:11 +0300

    Fixed wrong link. (after-vote)

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    path('<int:pk>/result/', views.ResultView.as_view(), name='result'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
